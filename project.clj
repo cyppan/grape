0d69b955fd9b7b221c64b702b48bd3b5153f6e14
Cyprien PANNIER <cyprien.pannier@gmail.com>
2016-05-16 22:28:24 +0200

    bump versions

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/cyppan/grappe"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cheshire "5.5.0"]
-                 [prismatic/schema "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [cheshire "5.6.1"]
+                 [prismatic/schema "1.1.1"]
                  [prismatic/plumbing "0.5.3"]
-                 [com.novemberain/monger "3.0.1"]
+                 [com.novemberain/monger "3.0.2"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [bidi "2.0.9"]]
   :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]])
