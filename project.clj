6cdda047f5e1d88052483a1d2458e5e734eb49e3
Cyprien Pannier <cyprien.pannier@gmail.com>
2016-10-26 17:54:01 +0200

    inc version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grape "0.1.2"
+(defproject grape "0.1.3"
   :description "The opinionated, data-first, REST, GraphQL and Falcor enabled API Clojure library"
   :url "https://github.com/cyppan/grape"
   :license {:name "Eclipse Public License"
