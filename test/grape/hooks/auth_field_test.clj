773dae53d688581c5b1db57e710108835d1f9765
Jean BOUDET <jean.boudet@yahoo.fr>
2017-12-12 16:11:49 +0100

    Allow to authorize role in the auth strategy

--- test/grape/hooks/auth_field_test.clj
+++ test/grape/hooks/auth_field_test.clj
@@ -1,1 +1,27 @@
-(ns grape.hooks.auth-field-test)
+(ns grape.hooks.auth-field-test
+  (:require [clojure.test :refer :all]
+            [grape.hooks.auth-field :as auth-field])
+  (:import (clojure.lang ExceptionInfo)))
+
+(deftest auth-strategy-with-no-role-field
+  (testing "Should not thrown an error"
+    (let [hook-pre-read-fn (:pre-read auth-field/hooks)
+          resource {:public-operations nil
+                    :auth-strategy     {:type       :field
+                                        :auth-field :account_id
+                                        :doc-field  :_id}}
+          request {:auth {:account_id "aaaaaaaaaaaaaaaaaaaaaaaa"}}]
+      (is (= {:find {:_id "aaaaaaaaaaaaaaaaaaaaaaaa"}}
+            (hook-pre-read-fn nil resource request nil))))))
+
+(deftest auth-strategy-with-role-field
+  (testing "Should thrown an error because bad role"
+    (let [hook-pre-read-fn (:pre-read auth-field/hooks)
+          resource {:public-operations nil
+                    :auth-strategy     {:type       :field
+                                        :auth-field :account_id
+                                        :doc-field  :_id
+                                        :has-role (fn [role] (= role "foo"))}}
+          request {:auth {:account_id "aaaaaaaaaaaaaaaaaaaaaaaa"
+                          :roles ["baz"]}}]
+      (is (thrown? ExceptionInfo (hook-pre-read-fn nil resource request nil))))))
