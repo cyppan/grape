43677b6ec55742cb0140c61cdcebad7b9eafb8fb
Cyprien Pannier <cyprien.pannier@gmail.com>
2021-04-03 00:34:17 +0200

    let data specify the Clojure version to use

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/BeOpinion/grape"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
                  [cheshire "5.10.0"]
                  [clj-time "0.15.2"]
                  [prismatic/schema "1.1.12"]
